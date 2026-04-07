59b990fdc300e16100c0066f25607cc552ef995f
Dissperss <subbotin.vladimir2018@ya.ru>
2026-04-07 11:50:24 +0300

    remove uppercase Container and Section

--- src/shared/ui/Section/Section.tsx
+++
@@ -1,8 +0,0 @@
-type SectionProps = {
-    children: React.ReactNode
-    className?: string
-}
-
-export const Section = ({ children, className }: SectionProps) => {
-    return <section className={className}>{children}</section>
-}
