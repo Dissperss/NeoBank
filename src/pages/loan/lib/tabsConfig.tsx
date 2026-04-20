9b764b5383160199341ed8c13cf1d97a8e918ae4
Dissperss <subbotin.vladimir2018@ya.ru>
2026-04-20 11:23:23 +0300

    feat: RatesTab

--- src/pages/loan/lib/tabsConfig.tsx
+++ src/pages/loan/lib/tabsConfig.tsx
@@ -1,8 +1,9 @@
 import { AboutCardTab } from '../ui/sections/tabsContent/aboutCardTab'
+import { RatesTab } from '../ui/sections/tabsContent/ratesAndConditionsTab'
 
 export const tabs = [
     { id: 'about', label: 'About card', content: <AboutCardTab /> },
-    { id: 'rates', label: 'Rates and conditions', content: 'afsafs' },
+    { id: 'rates', label: 'Rates and conditions', content: <RatesTab /> },
     { id: 'cashback', label: 'Cashback', content: 'afsafasfasfafasf' },
     { id: 'faq', label: 'FAQ', content: 'afsfasfafs' },
 ]
---
+++ src/pages/loan/ui/sections/tabsContent/ratesAndConditionsTab/index.ts
@@ -0,0 +1,1 @@
+export { RatesTab } from './RatesTab'
