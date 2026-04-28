350a247d29252ab5d21cf5ab1d04289ed11f7819
Dissperss <subbotin.vladimir2018@ya.ru>
2026-04-28 19:58:07 +0300

    feat: docker connected

--- src/shared/api/email/email.ts
+++ src/shared/api/email/email.ts
@@ -4,17 +4,15 @@ interface ApiResponse {
     message: string
 }
 
-export const subscribeEmail = async (email: string): Promise<ApiResponse> => {
+export const subscribeEmail = async (email: string): Promise<void> => {
     try {
-        const { data } = await emailClient.post<ApiResponse>('', {
+        const data = await emailClient.post<ApiResponse>('', {
             email,
         })
 
-        if (!data) {
+        if (data.status !== 200) {
             throw new Error('Invalid response: missing data')
         }
-
-        return data
     } catch (error) {
         console.error(error)
         throw new Error(
--- src/shared/api/email/instance.ts
+++ src/shared/api/email/instance.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
-import { EMAIL_API_BASE_URL, EMAIL_API_TIMEOUT } from '@/shared/config/email'
+import {
+    BASE_HOST,
+    EMAIL_API_BASE_URL,
+    EMAIL_API_TIMEOUT,
+} from '@/shared/config/email'
 
 export const emailClient = axios.create({
-    baseURL: EMAIL_API_BASE_URL,
+    baseURL: `${BASE_HOST}/${EMAIL_API_BASE_URL}`,
     timeout: EMAIL_API_TIMEOUT,
 })
